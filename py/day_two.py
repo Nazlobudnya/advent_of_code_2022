393eb63273af4ff4c762aabba85148573ed49ade
nazlobudnya <igorthegod@gmail.com>
2022-12-03 17:28:07 +0200

    try out python

---
+++ py/day_two.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+def one():
+    with open("../src/day_two_input.txt") as file:
+        input = file.read().strip().split("\n")
+
+    hand_mapping = {
+        "A": 1,
+        "B": 2,
+        "C": 3,
+        "X": 1,
+        "Y": 2,
+        "Z": 3
+    }
+
+    ans = 0
+
+    for play in input:
+        enemy, player = [hand_mapping[i] for i in play.split(" ")]
+
+        if (player - enemy) % 3 == 1:
+            ans += 6
+        elif player == enemy:
+            ans += 3
+
+        ans += player
+
+    print(ans)
+
+
+one()
